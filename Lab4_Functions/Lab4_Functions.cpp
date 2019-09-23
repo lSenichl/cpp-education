298b08e24d4b457e904b52bc02ee8b15c0aebd94
lSenichl <brshnkv.smn@mail.ru>
2019-09-23 19:33:29 +0300

    Lab4

---
+++ Lab4_Functions/Lab4_Functions.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+void quadro(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << i + 1 << " ^ 2 = " << i * i + 2 * i + 1 << std::endl;
+	}
+}
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	quadro(n);
+}
