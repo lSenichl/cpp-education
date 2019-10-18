72841fd14cc27941ce41a601860585a5e2adf05d
lSenichl <brshnkv.smn@mail.ru>
2019-10-18 23:14:56 +0300

    Lab7

--- Lab7_Classes/matrix.h
+++ Lab7_Classes/matrix.h
@@ -10,16 +10,10 @@ public:
 
 	matrix(int trows, int tcolumns, int * tmatr);
 
-	int getrows();
-	int getcolumns();
-	int* getmatr();
-
 	void print();
 	void input();
 
-	matrix sum(matrix matrtemp1);
-
-	matrix sum2(matrix matrtemp1, matrix matrtemp2);
+	matrix sum(matrix matrtemp1, matrix matrtemp2);
 
 protected:
 	int rows;
